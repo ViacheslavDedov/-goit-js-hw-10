60ba35ba2bd288e3f671075837c959c9ff85b243
ViacheslavDedov <dedoff.v@gmail.com>
2022-05-23 13:39:36 +0300

    refs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
+
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+    searchBox: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+  }
+  
+
+
